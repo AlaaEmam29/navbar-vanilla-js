dfd66d8c19fdb324b0759f8977f5c91c509bdcd5
AlaaShaban29 <alaashaban29_sd@fcis.edu.eg>
2022-04-28 21:30:32 +0200

    add

---
+++ js/main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const navToggle = document.querySelector(".nav-toggle"),
+links = document.querySelector(".links");
+navToggle.addEventListener("click",function() {
+ links.classList.toggle("show-links");
+
+})
